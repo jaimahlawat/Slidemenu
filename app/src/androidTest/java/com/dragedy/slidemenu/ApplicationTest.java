22d49b311db38b8b44b6b0207097da8fce75e9e0
Jai Mahlawat <jaimahlawat2@gmail.com>
2016-09-03 12:52:04 +0530

    Initial commit

---
+++ app/src/androidTest/java/com/dragedy/slidemenu/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.dragedy.slidemenu;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
